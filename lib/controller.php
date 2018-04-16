35e6a1ab41709c5edbbf33197c108de3a49e7b37
HackMan <chiappa24@gmail.com>
2018-04-16 09:38:59 -0400

    New error handling with exceptions for TCPCommunications and added a stub to interact with the MPI

--- lib/controller.php
+++ lib/controller.php
@@ -1,6 +1,8 @@
 <?php
 require 'pack.php';
 require 'TCPCommunications.php';
+require 'mpi.php';
+//require 'tmp.php';
 
 class Controller
 {
@@ -13,38 +15,57 @@ class Controller
 
 	public function process($card_num, $sum, $pid, $cvv)
 	{
+		$mpi = new MPI($card_num);
+		if ($mpi->Process() == MPI_STATUS[0])
+		{
+			return -1;	
+		}
+		
 		$this->get_login_info($this->login_file);
 		$this->port = $this->get_port_by_pid($pid);
 		$message = pack_message($card_num, $sum, $pid);
 		$packed_message = $message->pack();
 
-		$tcp = new TCPCommunications($this->host, $this->port, 5);
 		try
 		{
+			$tcp = new TCPCommunications($this->host, $this->port, 5);	
 			$tcp->Send(hex2bin($packed_message)); 
 		}
 		catch (Exception $e)
 		{
 			if ($e->getCode() == TIMEOUT)
-			SendReversal($card_num, $sum, $pid);
-			return -1;
+			{
+				try
+				{
+					SendReversal($card_num, $sum, $pid);
+					// Return cancaled operation
+				}
+				catch (Exception $e)
+				{
+					if ($e->getCode() == TIMEOUT)
+					{
+						// deferred operation
+						return -1;
+					}	
+				}
+				return -2;
+			}
 		}
 		$unpack = $message->unpack($tcp->Recive());
 		
 		$bitmap = "{\"bitmap\":".json_encode($message->getbitmap())."}\n";
-		$mti = json_encode($message->getmti());
+		$mti = "{\"mti\":".json_encode($message->getmti())."}\n";;
 		$fields = json_encode($message->getfields());
-
+		echo($mti."\n");
+		echo($bitmap."\n");
+		echo($fields."\n");
 		$fields = $message->getfields();	
 		return $fields['39'];
 	}
 
 	private function get_port_by_pid($pid)
 	{
-		$data = file_get_contents('oci.json');
-		$oci = json_decode($data, true);
-
-		$conn = oci_connect($this->username, $this->password);
+		$conn = oci_connect($this->username, $this->password, $this->get_connection_string());
 		if (!$conn) {
 			$e = oci_error();
 			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
@@ -53,7 +74,7 @@ class Controller
 		$stid = oci_parse($conn, 'SELECT LOCAL_PORT FROM TCP_TAB WHERE PID = '.$pid);
 		oci_execute($stid);
 
-		while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+		while ($row = oci_fetch_assoc($stid)){ 
 			foreach ($row as $item) {
 				return $item;
 			}
@@ -65,25 +86,14 @@ class Controller
 		$data = file_get_contents('oci.json');
 		$oci = json_decode($data, true);
 		$this->json_to_oracle($oci);
-		print($this->connection_string);
 		return $this->connection_string; 
-	}
-
-	private function get_login_info($file)
-	{
-		$data = file_get_contents($file);	
-		$log_info = json_decode($data, true);
-
-		$this->username = $log_info['username'];
-		$this->password = $log_info['password'];		
-		$this->host = $log_info['host'];
 	}
 
 	private function json_to_oracle($oci)
 	{
 		if(is_array($oci))
 		{
-			foreach($oci as $key=>$value)
+			foreach($oci as $key => $value)
 			{
 				if(is_array($value) || is_object($value))
 				{
@@ -97,6 +107,16 @@ class Controller
 				}
 			}
 		}
+	}
+
+	private function get_login_info($file)
+	{
+		$data = file_get_contents($file);	
+		$log_info = json_decode($data, true);
+
+		$this->username = $log_info['username'];
+		$this->password = $log_info['password'];		
+		$this->host = $log_info['host'];
 	}
 
 }
